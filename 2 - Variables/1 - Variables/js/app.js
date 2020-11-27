19d5f2f4b12b25c9b35b9b30214d00e147bde7f8
Ariel Pont <arielpont@hotmail.com>
2020-11-26 22:53:08 -0300

    Add some new comments

--- 2 - Variables/1 - Variables/js/app.js
+++ 2 - Variables/1 - Variables/js/app.js
@@ -1,3 +1,6 @@
 "use strict"
 
 var nombre = "Sergio";
+
+//Variables globales y locales
+//Las variables son destructivas, o sea que se reemplazan los valores.
--- 2 - Variables/2 - Contenedores let/js/app.js
+++ 2 - Variables/2 - Contenedores let/js/app.js
@@ -6,12 +6,15 @@ console.log(nombre)
 
 function saludo() {
   let nombre = 'yacafx';
-    console.log(nombre)
-    
- let edad = 34; 
+  console.log(nombre)
+
+  let edad = 34;
   console.log(edad)
 }
 
- console.log(edad)
+console.log(edad)
 
 saludo();
+
+//Introduciendo el concepto de "scope" o alcance.
+//Introduciendo la palabra reservada "let" para crear variables dentro de las funciones, o sea variables locales.
